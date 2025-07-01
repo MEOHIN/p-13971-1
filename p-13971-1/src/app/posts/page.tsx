73f230211eb7a8b8fd64913d55191d58890ea850
meohin <meohin.meo@gmail.com>
2025-07-01 15:10:46 +0900

    refactor(posts): 타입 수정 및 로딩 메시지 렌더링 위치 변경

--- p-13971-1/src/app/posts/page.tsx
+++ p-13971-1/src/app/posts/page.tsx
@@ -3,24 +3,25 @@
 import { useEffect, useState } from "react";
 
 export default function Page() {
-    const [posts, setPosts] = useState<{id: Number; title: string}[]>([]);
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/api/v1/posts")
-        .then((res) => res.json())
-        .then(setPosts);
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((res) => res.json())
+      .then(setPosts);
+  }, []);
 
-    return(
-        <>
-            <h1>글 목록</h1>
-            
-            <ul>
-                {posts.length == 0 && <li>로딩중...</li>}
-                {posts.map((posts) => (
-                    <li key={posts.id}>{posts.title}</li>
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <h1>글 목록</h1>
+
+      {posts.length == 0 && <div>로딩중...</div>}
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
