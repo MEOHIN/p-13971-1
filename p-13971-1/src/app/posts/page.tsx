bf6fdc98dee79c6829be6ea0a107e3043b28e8d6
meohin <meohin.meo@gmail.com>
2025-07-01 14:40:45 +0900

    fix typo

--- p-13971-1/src/app/posts/page.tsx
+++ p-13971-1/src/app/posts/page.tsx
@@ -1,11 +1,12 @@
 export default function Page() {
-    return
-    <>
-        <h1>글 목록</h1>
-        
-        <ul>
-            <li>명언1</li>
-            <li>명언2</li>
-        </ul>
-    </>
+    return(
+        <>
+            <h1>글 목록</h1>
+            
+            <ul>
+                <li>명언1</li>
+                <li>명언2</li>
+            </ul>
+        </>
+    );
 }
