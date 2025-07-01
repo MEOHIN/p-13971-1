5dc4efdf9b187c3a9b3e90f3c30789a90e2eb9f3
meohin <meohin.meo@gmail.com>
2025-07-01 15:08:29 +0900

    feat(posts): 서버에서 글 목록을 불러오는 기능 추가

--- p-13971-1/src/app/posts/page.tsx
+++ p-13971-1/src/app/posts/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Page() {
+    const [posts, setPosts] = useState<{id: Number; title: string}[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/v1/posts")
+        .then((res) => res.json())
+        .then(setPosts);
+    }, []);
+
     return(
         <>
             <h1>글 목록</h1>
             
             <ul>
-                <li>명언1</li>
-                <li>명언2</li>
+                {posts.length == 0 && <li>로딩중...</li>}
+                {posts.map((posts) => (
+                    <li key={posts.id}>{posts.title}</li>
+                ))}
             </ul>
         </>
     );
