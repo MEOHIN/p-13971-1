e38dec29a97fd2e7ef8c9c2e8375eaae49f503d7
meohin <meohin.meo@gmail.com>
2025-07-02 10:27:13 +0900

    feat: apiFetch에 옵션 추가하고 JSON 요청 처리 자동 설정

--- p-13971-1/src/lib/backend/client.ts
+++ p-13971-1/src/lib/backend/client.ts
@@ -1,5 +1,11 @@
 const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const apiFetch = (url: string) => {
+export const apiFetch = (url: string, options?: RequestInit) => {
+  if(options?.body) {
+    options.headers = {
+      ...options.headers,
+      "Content-Type": "application/json; charset=utf-8",
+    }
+  }
   return fetch(`${NEXT_PUBLIC_API_BASE_URL}${url}`).then((res) => res.json());
 };
